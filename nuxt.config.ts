678ebdb60dead607f8eb376c61862c2911ef56ab
Fabrício Pinto Ferreira <pferreirafabricio@gmail.com>
2023-08-09 20:48:46 -0300

    feat: add working example in dev mode

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,27 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import vuetify from "vite-plugin-vuetify";
+
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  build: {
+    transpile: ["vuetify"],
+  },
+  modules: [
+    // eslint-disable-next-line require-await
+    async (_options, nuxt) => {
+      nuxt.hooks.hook("vite:extendConfig", (config) =>
+        // @ts-ignore
+        config.plugins.push(vuetify()),
+      );
+    },
+  ],
+  css: [
+    "vuetify/lib/styles/main.sass",
+    "@mdi/font/css/materialdesignicons.min.css",
+  ],
+  vite: {
+    ssr: {
+      noExternal: ["vuetify"],
+    },
+  },
 })
